7f4d578acc6f4b8f30103275568b6ad33f5b1985
Jacques Fuentes <jpfuentes2@gmail.com>
2012-12-12 22:44:14 -0500

    Added bower manifest and grunt.js config

---
+++ grunt.js
@@ -0,0 +1,45 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'app/public/css/main.css': 'app/sass/main.sass'
+        }
+      },
+      options: {
+        'compass': true
+      }
+    },
+
+
+    coffee: {
+      compile: {
+        files: {
+          'public/scripts/*.js': 'src/*.coffee'
+        }
+      }
+    },
+
+
+    watch: {
+      coffee: {
+        files: ['app/coffee/*'],
+        tasks: 'coffee'
+      },
+      sass: {
+        files: ['app/sass/*'],
+        tasks: 'sass'
+      }
+    }
+  });
+
+  // Load necessary plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+  grunt.registerTask('default', 'sass coffee');
+
+};
